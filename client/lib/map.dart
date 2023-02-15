3272abc4f00d9c0a8e61a9be47544befe72c50db
Oscar Lund Ramstad <oramstad1@gmail.com>
2023-02-15 19:18:20 +0100

    Add polygon and check if a point is inside

--- client/lib/map.dart
+++ client/lib/map.dart
@@ -20,8 +20,10 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   final Completer<GoogleMapController> _controller = Completer();
-  final Set<Polyline> _polylines = {};
-  LatLng _smartphonePosition = const LatLng(59.913868, 10.752245); //Set to Oslo by default
+  final Set<Polygon> _polygon = {};
+  List<LatLng> _roomCorners = [];
+  // Set to room @ IFI by default
+  LatLng _smartphonePosition = const LatLng(59.944174, 10.719388);
   final double _zoomLevel = 15.0;
   final Set<Message> _messages = {};
 
@@ -30,24 +32,24 @@ class _MapState extends State<Map> {
   @override
   void initState() {
     super.initState();
-    _createPolylines();
+    _createPolygon();
     _createSocket();
   }
 
-  void _createPolylines() {
-    const LatLng startPosition = LatLng(59.930566, 10.715629);
-    List<LatLng> locations = [
-      startPosition,
-      const LatLng(59.930417, 10.715865),
-      const LatLng(59.930729,10.716638),
-      const LatLng(59.930878, 10.716406),
-      startPosition
+  void _createPolygon() {
+    // Room @ IFI
+    _roomCorners = [
+      const LatLng(59.944211697376325, 10.719388984143734),
+      const LatLng(59.944176937772774, 10.719477161765099),
+      const LatLng(59.944117493728605, 10.71938395500183),
+      const LatLng(59.94415074210543, 10.71929544210434)
     ];
 
-    _polylines.add(
-        Polyline(
-            polylineId: const PolylineId("1"),
-            points: locations
+    _polygon.add(
+        Polygon(
+            polygonId: const PolygonId("1"),
+            points: _roomCorners,
+            fillColor: Colors.redAccent
         )
     );
 
@@ -76,7 +78,12 @@ class _MapState extends State<Map> {
 
   Future<void> _sendData() async {
     final id = Increment.id;
-    final message = Message(id: id, position: _smartphonePosition, status: "Outside dangerous space", dateTime: DateTime.now());
+    final message = Message(
+        id: id,
+        position: _smartphonePosition,
+        status: _isInsidePolygon(_smartphonePosition, _roomCorners) ? "Inside dangerous space" : "Outside dangerous space",
+        dateTime: DateTime.now()
+    );
     _messages.add(message);
 
     _socket.add(utf8.encode(message.toString()));
@@ -90,11 +97,15 @@ class _MapState extends State<Map> {
       children: [
         Scaffold(
           body: GoogleMap(
+            onTap: (LatLng pos) {
+              print("Position is: " + pos.toString());
+              print("Inside critial area: " + _isInsidePolygon(pos, _roomCorners).toString());
+            },
             onMapCreated: (GoogleMapController googleMapController) {
               _controller.complete(googleMapController);
             },
             myLocationEnabled: true,
-            polylines: _polylines,
+            polygons: _polygon,
             myLocationButtonEnabled: false,
             compassEnabled: true,
             zoomGesturesEnabled: true,
@@ -150,6 +161,40 @@ class _MapState extends State<Map> {
     }
 
     return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+  }
+
+  bool _isInsidePolygon(LatLng point, List<LatLng> polygon) {
+    int intersectCount = 0;
+
+    for (int i = 0; i < polygon.length - 1; i++) {
+      if (rayCasting(point, polygon[i], polygon[i + 1])) {
+        intersectCount++;
+      }
+    }
+    // If odd is returned, the point is inside the polygon
+    return ((intersectCount % 2) == 1);
+  }
+
+  bool rayCasting(LatLng point, LatLng a, LatLng b) {
+    double aY = a.latitude;
+    double aX = a.longitude;
+
+    double bY = b.latitude;
+    double bX = b.longitude;
+
+    double pY = point.latitude;
+    double pX = point.longitude;
+
+    if ((aY > pY && bY > pY) || (aY < pY && bY < pY) || (aX < pX && bX < pX)) {
+      return false;
+    }
+
+    double m = (aY - bY) / (aX - bX);
+    double n = (-aX) * m + aY;
+
+    double x = (pY - n) / m;
+
+    return x > pX;
   }
 
 }
