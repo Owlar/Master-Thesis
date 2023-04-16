8d532c4411480f98b82745c7fd245746130312da
Oscar Lund Ramstad <oramstad1@gmail.com>
2023-04-16 04:56:27 +0200

    Minor refactor and cleanup

--- client/lib/map.dart
+++ client/lib/map.dart
@@ -22,18 +22,106 @@ class _MapState extends State<Map> {
   final Completer<GoogleMapController> _controller = Completer();
   final double _zoomLevel = 14.0;
 
+  late GoogleMapController _googleMapController;
   late LatLng _smartphonePosition;
   late Socket _socket;
 
   Set<Status> _messages = {};
   bool _warned = false;
 
-  late GoogleMapController _googleMapController;
-
 
   @override
   void initState() {
     super.initState();
+  }
+
+
+  Future<void> _stop() async {
+    _socket.close();
+    setState(() {
+      _messages = {};
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+                color: _warned ? Colors.redAccent : Colors.greenAccent,
+                width: 7.0
+            )
+        ),
+        child: Stack(
+            children: [
+              Scaffold(
+                body: GoogleMap(
+                  onTap: (LatLng pos) {
+                    print(pos.toString());
+                  },
+                  onMapCreated: (GoogleMapController googleMapController) {
+                    _controller.complete(googleMapController);
+                    _googleMapController = googleMapController;
+                  },
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: false,
+                  compassEnabled: true,
+                  zoomGesturesEnabled: true,
+                  zoomControlsEnabled: false,
+                  initialCameraPosition: CameraPosition(
+                      target: const LatLng(59.94416434370449, 10.719385296106339),
+                      zoom: _zoomLevel
+                  ),
+                ),
+                floatingActionButton: _messages.isEmpty ? FloatingActionButton.large (
+                  onPressed: () => _start(),
+                  child: const Icon(Icons.location_on_outlined, size: 60),
+                ) : FloatingActionButton.large (
+                    onPressed: () => _stop(),
+                    backgroundColor: Colors.redAccent,
+                    child: const Icon(Icons.location_off_outlined, size: 60)
+                ),
+              )
+            ]
+        )
+    );
+  }
+
+
+  Future<void> _start() async {
+    final position = await _getPosition();
+    setState(() {
+      _smartphonePosition = LatLng(position.latitude, position.longitude);
+    });
+    await _createSocket().then((value) {
+      _listenOnSocket();
+      _animateCamera();
+      _showSnackBar(_smartphonePosition.toString());
+      _updateLatestPosition();
+    }, onError: (e) => _showSnackBar("Could not connect to server!"));
+  }
+
+
+  // Source: https://pub.dev/packages/geolocator (26.01.2023)
+  Future<Position> _getPosition() async {
+    bool isServiceEnabled;
+    LocationPermission permission;
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) return Future.error("Location is not enabled!");
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permissions were denied!");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("Location permissions are denied forever, check settings!");
+    }
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
   }
 
 
@@ -51,8 +139,10 @@ class _MapState extends State<Map> {
     if (androidInfo.isPhysicalDevice || iOSInfo.isPhysicalDevice) {
       ip = serverPublicIp;
     }
-    _socket = await Socket.connect(ip, 8080);
+    _socket = await Socket.connect(ip, 8080)
+        .timeout(const Duration(seconds: 2));
   }
+
 
 
   Future<void> _listenOnSocket() async {
@@ -71,119 +161,6 @@ class _MapState extends State<Map> {
     });
   }
 
-  Future<void> _animateCamera() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(
-            target: _smartphonePosition,
-            zoom: _zoomLevel
-        )
-    ));
-  }
-
-
-  Future<void> _sendData(String id) async {
-    final status = Status(
-        id: id,
-        position: _smartphonePosition.latitude.toString() + "," + _smartphonePosition.longitude.toString(),
-    );
-    //For toggling position recording on/off
-    setState(() {
-      _messages.add(status);
-    });
-
-    _socket.add(utf8.encode(status.toString()));
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: _warned ? Colors.redAccent : Colors.greenAccent,
-          width: 7.0
-        )
-      ),
-      child: Stack(
-          children: [
-            Scaffold(
-              body: GoogleMap(
-                onTap: (LatLng pos) {
-                  print(pos.toString());
-                },
-                onMapCreated: (GoogleMapController googleMapController) {
-                  _controller.complete(googleMapController);
-                  _googleMapController = googleMapController;
-                },
-                myLocationEnabled: true,
-                myLocationButtonEnabled: false,
-                compassEnabled: true,
-                zoomGesturesEnabled: true,
-                zoomControlsEnabled: false,
-                initialCameraPosition: CameraPosition(
-                    target: const LatLng(59.94416434370449, 10.719385296106339),
-                    zoom: _zoomLevel
-                ),
-              ),
-              floatingActionButton: _messages.isEmpty ? FloatingActionButton.large (
-                  onPressed: () => _start(),
-                  child: const Icon(Icons.location_on_outlined, size: 60),
-              ) : FloatingActionButton.large (
-                  onPressed: () => _stop(),
-                  backgroundColor: Colors.redAccent,
-                  child: const Icon(Icons.location_off_outlined, size: 60)
-              ),
-            )
-          ]
-        )
-    );
-  }
-
-
-  Future<void> _start() async {
-    final position = await _getPosition();
-    setState(() {
-      _smartphonePosition = LatLng(position.latitude, position.longitude);
-    });
-    // TODO: Give message if server is down
-    await _createSocket().then((value) {
-      _listenOnSocket();
-      _animateCamera();
-      _showSnackBar(_smartphonePosition.toString());
-      _streamLatestPosition();
-    });
-  }
-
-
-  // Source: https://pub.dev/packages/geolocator (26.01.2023)
-  Future<Position> _getPosition() async {
-    bool isServiceEnabled;
-    LocationPermission permission;
-    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!isServiceEnabled) return Future.error("Location is not enabled!");
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error("Location permissions were denied!");
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error("Location permissions are denied forever, check settings!");
-    }
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-  }
-
-
-  void _showSnackBar(String text) {
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content: Text(text)
-        )
-    );
-  }
-
 
   void _warn() {
     _showSnackBar("You are currently inside a critical area!");
@@ -198,7 +175,39 @@ class _MapState extends State<Map> {
   }
 
 
-  Future<void> _streamLatestPosition() async {
+  void _showSnackBar(String text) {
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(text))
+    );
+  }
+
+
+  Future<void> _sendData(String id) async {
+    final status = Status(
+      id: id,
+      position: _smartphonePosition.latitude.toString() + "," + _smartphonePosition.longitude.toString(),
+    );
+    // To keep track of position recording toggle
+    setState(() {
+      _messages.add(status);
+    });
+    _socket.add(utf8.encode(status.toString()));
+  }
+
+
+
+  Future<void> _animateCamera() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: _smartphonePosition,
+            zoom: _zoomLevel
+        )
+    ));
+  }
+
+
+  Future<void> _updateLatestPosition() async {
     const locationSettings = LocationSettings(
         accuracy: LocationAccuracy.best,
         distanceFilter: 1
@@ -208,13 +217,6 @@ class _MapState extends State<Map> {
     });
   }
 
-
-  Future<void> _stop() async {
-    _socket.close();
-    setState(() {
-      _messages = {};
-    });
-  }
 
 }
 
